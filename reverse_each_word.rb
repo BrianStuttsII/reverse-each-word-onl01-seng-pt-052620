2af9d8f3908d9e35f5909d01faf8dca9ed1a2644
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-09 19:28:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,11 @@
 def reverse_each_word(string)
   
-
-
-  
-  array = string.split(" ") #turn string into an array
-  test_array = []
+array = string.split(" ") #turn string into an array
+  sentence_reverse = []
   array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+    sentence_reverse << string.reverse #reverse each word in the array
   end
-  test_array.join(" ")
+  sentence_reverse.join(" ")
 end
 
 
