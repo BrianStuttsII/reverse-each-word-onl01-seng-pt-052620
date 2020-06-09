64963db1472be5d1a223fcc3f57acfc8a9ebe73c
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-09 19:25:35 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  
+  sentence1 = string.split(" ")
+   sentence2 []
+  sentence1.each do |string|
+    sentence2 << string.reverse 
+  end
+   sentence2.join(" ")
+end
+
+  
+  
